a1ee1c87782b38fef519aa7054903a967ebb5fdc
kreativae <91092288+RedOceam@users.noreply.github.com>
2021-09-24 15:08:22 -0300

    Register Admin Model Visualizer
    Create the ModelAdmin for the three models created

--- MyGameList/Blog/admin.py
+++ MyGameList/Blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from Blog import models
 
-# Register your models here.
+@admin.register(models.Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+
+@admin.register(models.Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+
+@admin.register(models.Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("name", )
